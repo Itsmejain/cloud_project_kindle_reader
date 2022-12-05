76b62795466d9561a7730f64cd17f0591a1782bf
Itsmejain <shubhamjain.ofcl@gmail.com>
2022-12-05 18:40:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+
+app = Flask(__name__,template_folder="./templates")
+
+
+#route home api
+@app.route("/")
+def home():
+ return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+ app.run(debug=True,port=5000)
